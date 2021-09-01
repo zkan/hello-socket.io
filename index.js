ef8e663ddcda1eaa701ff18dc6be3108c41f6052
zkan <zkan.cs@gmail.com>
2021-09-01 20:02:51 +0700

    Integrate Socket.IO to server :bear:

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 
+const { Server } = require('socket.io')
+const io = new Server(server)
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected')
 })
 
 server.listen(3000, () => {
